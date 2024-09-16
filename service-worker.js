2f67952ffb7141809f4c92dbcbb4adfa10b45b87
practicallyplaying <128937216+practicallyplaying@users.noreply.github.com>
2024-09-15 23:32:03 -0500

    Added PWA setup and icons

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,23 @@
-const CACHE_NAME = "pp-library-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/images/icon-192x192.png",
-  "/images/icon-512x512.png"
-];
 
-// Install the service worker
-self.addEventListener("install", function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+    caches.open('pp-library-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/Images/icon-192x192.png',
+        '/Images/icon-512x512.png',
+        '/Images/pirate-icon.png'
+      ]);
     })
   );
 });
 
-// Cache and return requests
-self.addEventListener("fetch", function(event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
-    })
-  );
-});
-
-// Update the service worker
-self.addEventListener("activate", function(event) {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
     })
   );
 });
