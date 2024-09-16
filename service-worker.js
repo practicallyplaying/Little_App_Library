313a9568ab3e31f36d4cf06eaaf877372ac0813f
practicallyplaying <128937216+practicallyplaying@users.noreply.github.com>
2024-09-16 12:16:54 -0500

    Updated manifest.json and service worker for PWA

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,47 @@
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('pp-library-cache').then(function(cache) {
-      return cache.addAll([
+    const CACHE_NAME = 'practically-playing-cache-v1';
+    const urlsToCache = [
         '/',
         '/index.html',
         '/manifest.json',
-        '/Images/icon-192x192.png',
-        '/Images/icon-512x512.png',
-        '/Images/pirate-icon.png'
-      ]);
-    })
-  );
-});
+        '/Images/icon-192x192-pirate.png',
+        '/Images/icon-512x512-pirate.png'
+    ];
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+    self.addEventListener('install', event => {
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(cache => {
+                    console.log('Opened cache');
+                    return cache.addAll(urlsToCache);
+                })
+        );
+    });
+
+    self.addEventListener('fetch', event => {
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => {
+                    if (response) {
+                        return response; // Return the cached version
+                    }
+                    return fetch(event.request); // Perform network request if not in cache
+                })
+        );
+    });
+
+    self.addEventListener('activate', event => {
+        const cacheWhitelist = [CACHE_NAME];
+        event.waitUntil(
+            caches.keys().then(cacheNames => {
+                return Promise.all(
+                    cacheNames.map(cacheName => {
+                        if (cacheWhitelist.indexOf(cacheName) === -1) {
+                            return caches.delete(cacheName); // Delete old cache
+                        }
+                    })
+                );
+            })
+        );
+    });
+    
